74b572adfd27df35f8ac54420ae19aaaa8b94ef7
jseteny <jseteny@gmail.com>
2017-09-27 16:59:32 +0200

    for (sorIndex <- tábla.zipWithIndex) yield         for (cellaIndex <- sorIndex._1.zipWithIndex) yield           (cellaIndex._1, sorIndex._2, cellaIndex._2) match {
                case (_, cx, cy) if cx == x && cy == y =>
                  Akna
    
                case (Szám(n), cx, cy) =>
                  if (Math.abs(cx - x) <= 1 && Math.abs(cy - y) <= 1) Szám(n + 1)
                  else Szám(n)
    
                case (c, _, _) =>
                  c
              }
          for (sorIndex <- tábla.zipWithIndex) yield
            for (cellaIndex <- sorIndex._1.zipWithIndex) yield
              (cellaIndex._1, sorIndex._2, cellaIndex._2) match {
    
                case (_, cx, cy) if cx == x && cy == y =>
                  Akna
    
                case (Szám(n), cx, cy) =>
                  if (Math.abs(cx - x) <= 1 && Math.abs(cy - y) <= 1) Szám(n + 1)
                  else Szám(n)
    
                case (c, _, _) =>
                  c
              }

--- Szeptember2017/src/main/scala/d/negyedik/het/D2 aknakereso generáló.scala
+++ Szeptember2017/src/main/scala/d/negyedik/het/D2 aknakereso generáló.scala
@@ -37,6 +37,20 @@ object AknakeresőGeneráló {
     }
 
     def rakd(x: Int, y: Int, tábla: Tábla): Tábla =
+      for (sorIndex <- tábla.zipWithIndex) yield
+        for (cellaIndex <- sorIndex._1.zipWithIndex) yield
+          (cellaIndex._1, sorIndex._2, cellaIndex._2) match {
+
+            case (_, cx, cy) if cx == x && cy == y =>
+              Akna
+
+            case (Szám(n), cx, cy) =>
+              if (Math.abs(cx - x) <= 1 && Math.abs(cy - y) <= 1) Szám(n + 1)
+              else Szám(n)
+
+            case (c, _, _) =>
+              c
+          }
 
 
   }
