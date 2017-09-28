6f75c97ebbd37b9507a36f06b1b05874e5415033
jseteny <jseteny@gmail.com>
2017-09-28 19:12:43 +0200

    írdKiEgymásMellé magyarázat

--- Szeptember2017/src/main/scala/view/AknakeresőKonzolon.scala
+++ Szeptember2017/src/main/scala/view/AknakeresőKonzolon.scala
@@ -1,60 +1,54 @@
 package view
 
-import d.negyedik.het.AknakeresőGeneráló._
+import d.negyedik.het.AknakeresőGeneráló.{Akna, Szám, Tábla}
 
 object AknakeresőKonzolon {
 
   def írdKiEgymásMellé(táblák: Tábla*): Unit = {
-    írdKiEgymásMellé(táblák.toList)
-  }
-
-  def írdKiEgymásMellé(táblák: List[Tábla]): Unit = {
 
     def cellánkéntString(tábla: Tábla): List[List[String]] = {
       for (sor <- tábla) yield
         for (cella <- sor) yield cella match {
           case Akna => "*"
           case Szám(n) => n.toString
-          case TakartAkna => "."
-          case TakartSzám(_) => "."
         }
     }
 
     val elválasztó = "   "
 
     println(
-      táblák.map((t:Tábla) => cellánkéntString(t))
+      táblák.map(cellánkéntString)
         .transpose.map(_.map(_.mkString).mkString(elválasztó)).mkString("\n")
     )
 
     /*
     val elválasztó = "   "
 
-    val aTábla = List(List(1, 2), List(3, 4))
+    val as = List(List(1, 2), List(3, 4))
     // 12
     // 34
 
-    val bTábla = List(List(6, 7), List(8, 9))
+    val bs = List(List(6, 7), List(8, 9))
     // 67
     // 89
 
-    val táblák = List(aTábla, bTábla)
-    táblák: List[List[List[Int]]] = List(List(List(1, 2), List(3, 4)), List(List(6, 7), List(8, 9)))
+    val t = List(as,bs)
+    t: List[List[List[Int]]] = List(List(List(1, 2), List(3, 4)), List(List(6, 7), List(8, 9)))
 
-    táblák.transpose
+    t.transpose
     res8: List[List[List[Int]]] = List(List(List(1, 2), List(6, 7)), List(List(3, 4), List(8, 9)))
 
-    táblák.transpose.mkString("\n")
+    t.transpose.mkString("\n")
     res9: String =
     List(List(1, 2), List(6, 7))
     List(List(3, 4), List(8, 9))
 
-    táblák.transpose.map(_.mkString(elválasztó)).mkString("\n")
+    t.transpose.map(_.mkString(elválasztó)).mkString("\n")
     res10: String =
     List(1, 2)   List(6, 7)
     List(3, 4)   List(8, 9)
 
-    táblák.transpose.map(_.map(_.mkString).mkString(elválasztó)).mkString("\n")
+    t.transpose.map(_.map(_.mkString).mkString(elválasztó)).mkString("\n")
     res11: String =
     12   67
     34   89
