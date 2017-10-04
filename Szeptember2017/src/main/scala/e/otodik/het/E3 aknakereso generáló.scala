88c5b04c63b721b045651f4810cf7469ff46a1f8
jseteny <jseteny@gmail.com>
2017-10-04 02:33:53 +0200

    Viszkok Tamás megoldása, E2 összes megoldás - öntsünk tiszta vizet a vödörbe.sc

---
+++ Szeptember2017/src/main/scala/e/otodik/het/E3 aknakereso generáló.scala
@@ -0,0 +1,42 @@
+package e.otodik.het
+
+object AknakeresőGeneráló {
+
+  trait Cella
+
+  case object Akna extends Cella
+
+  case class Szám(n: Int) extends Cella
+
+  type Tábla = List[List[Cella]]
+
+
+  def main(args: Array[String]): Unit = {
+
+    val üres = List(
+      List(Szám(0), Szám(0), Szám(0)),
+      List(Szám(0), Szám(0), Szám(0)),
+      List(Szám(0), Szám(0), Szám(0))
+    )
+
+    írdKi(rakd(0,1,rakd(0, 0, üres)))
+    írdKi(rakd(0, 1, üres))
+    írdKi(rakd(1, 1, üres))
+
+
+    def írdKi(tábla: Tábla): Unit = {
+      for (sor <- tábla) {
+        println
+        for (cella <- sor) cella match {
+          case Akna => print("*")
+          case Szám(n) => print(n)
+        }
+      }
+      println
+    }
+
+    def rakd(x: Int, y: Int, tábla: Tábla): Tábla =
+
+
+  }
+}
