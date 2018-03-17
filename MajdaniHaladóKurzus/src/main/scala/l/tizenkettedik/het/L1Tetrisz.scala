eac987483e15eca55ec53202d494ef409eeda66c
jseteny <jseteny@gmail.com>
2018-03-17 22:37:54 +0100

    IndexOutOfBoundsException: 7 a földetért metódusban

--- MajdaniHaladóKurzus/src/main/scala/l/tizenkettedik/het/L1Tetrisz.scala
+++ MajdaniHaladóKurzus/src/main/scala/l/tizenkettedik/het/L1Tetrisz.scala
@@ -5,13 +5,15 @@ import java.lang.Math.{max, min}
 object L1Tetrisz {
 
   type Pálya = List[List[String]]
-  type Lépés = (Pálya, List[Pálya], (Int, Int), Pálya, List[Pálya]) => (Pálya, List[Pálya], (Int, Int), Pálya, List[Pálya])
+  type Lépés = (List[Pálya], (Int, Int), Pálya, List[Pálya]) => (List[Pálya], (Int, Int), Pálya, List[Pálya])
 
   val pályaMagasság = 6
   val kezdőPont: (Int, Int) = (2, 0)
 
   val felsőSor = List("X", " ", " ", " ", " ", "X")
   val alsóSor = List("X", "X", "X", "X", "X", "X")
+
+  private val üresPálya = List.fill(pályaMagasság)(felsőSor) ::: List(alsóSor)
 
 
   def main(args: Array[String]): Unit = {
@@ -20,10 +22,9 @@ object L1Tetrisz {
       List(List("A", "B"), List("C", "D")),
       List(List(null, "E"), List(null, "F"))
     )
-    val pálya = List.fill(pályaMagasság)(felsőSor) ::: List(alsóSor)
 
     írdKiEgymásMellé(
-      lépj(elemek, pálya,
+      lépj(elemek, üresPálya,
 
         semmi, forduljBalra, forduljBalra, jobbra,
         ejtsd,
@@ -38,29 +39,15 @@ object L1Tetrisz {
 
   def lépj(elemek: List[Pálya], pálya: Pálya, lépések: Lépés*): List[Pálya] = {
     val elsőPálya = rakdRá(elemek.head, kezdőPont, pálya)
-
     val (_, _, _, pályaLista) = lépések.foldLeft((elemek, kezdőPont, pálya, List(elsőPálya))) {
 
-      case ((e :: többiElem, (x, y), háttér, pályák), `ejtsd`) =>
-        ejtsdLe(e, többiElem, kezdőPont, háttér, pályák)
-
-      case ((e1 :: többiElem, hova, háttér, pályák), lépés) if földetért(e1, hova, háttér) =>
-        következő(e1, többiElem, hova, háttér, pályák)
-
-      case ((e :: et, (x, y), háttér, pályák), lépés) =>
-        // Vagy az elem, vagy x vagy y módosul
-        val (módosítottElem, (újX, újY)) = lépés(e, (x, y))
-
-        val hova = (
-          min(max(újX, 1), pálya.head.size - 3),
-          min(max(újY + 1, 0), pálya.size - 2)
-        )
-        val újPálya = rakdRá(módosítottElem, hova, háttér)
-        (módosítottElem :: et, hova, háttér, újPálya :: pályák)
+      case ((ek, (x, y), háttér, pályák), lépés) =>
+        lépés(ek, (x, y), háttér, pályák)
 
       case ((Nil, (x, y), háttér, pályák), _) =>
         (Nil, (x, y), háttér, pályák)
     }
+
     pályaLista.reverse.zip(semmi :: lépések.toList).map {
       case (p, `forduljBalra`) => p ::: List(List("  ↺   "))
       case (p, `forduljJobbra`) => p ::: List(List("  ↻  "))
@@ -71,63 +58,85 @@ object L1Tetrisz {
     }
   }
 
-  def földetért(elem: Pálya, hol: (Int, Int), háttér: Pálya): Boolean = {
-    val alsóSor = elem.last
-    alsóSor.zipWithIndex.exists {
-      case (null, _) =>
-        false
+  //noinspection ScalaUnusedSymbol
+  val földetért: (List[Pálya], (Int, Int), Pálya) => Boolean = {
+    case (Nil, hol: (Int, Int), háttér: Pálya) =>
+      false
 
-      case (_, index) =>
-        val (x, y) = hol
-        háttér(y + elem.size)(x + index) != " "
-    }
+    case (elem :: _, hol: (Int, Int), háttér: Pálya) =>
+      val alsóSor = elem.last
+      alsóSor.zipWithIndex.exists {
+        case (null, _) =>
+          false
+
+        case (_, index) =>
+          val (x, y) = hol
+          val strings = háttér(y + elem.size)
+          strings(x + index) != " "
+      }
   }
 
-  val forduljBalra: Lépés = (elem, hova) => (elem.map(_.reverse).transpose, hova)
+  val forduljBalra: Lépés = {
+    case (e :: többielem, (x, y), háttér: Pálya, pályák: List[Pálya]) =>
+      tovább(e.map(_.reverse).transpose :: többielem, (x, y + 1), háttér, pályák)
+  }
 
-  val forduljJobbra: Lépés = (elem, hova) => (elem.transpose.map(_.reverse), hova)
+  val forduljJobbra: Lépés = {
+    case (e :: többielem, (x, y), háttér: Pálya, pályák: List[Pálya]) =>
+      tovább(e.transpose.map(_.reverse) :: többielem, (x, y + 1), háttér, pályák)
+  }
 
   val jobbra: Lépés = {
-    case (elem, (x, y)) => (elem, (x + 1, y))
+    case (e :: többielem, (x, y), háttér: Pálya, pályák: List[Pálya]) =>
+      tovább(e :: többielem, (x + 1, y + 1), háttér, pályák)
   }
 
   val balra: Lépés = {
-    case (elem, (x, y)) => (elem, (x - 1, y))
+    case (e :: többielem, (x, y), háttér: Pálya, pályák: List[Pálya]) =>
+      tovább(e :: többielem, (x - 1, y + 1), háttér, pályák)
   }
 
   val semmi: Lépés = {
-    case (elem, (x, y)) => (elem, (x, y))
+    case (e :: többielem, (x, y), háttér: Pálya, pályák: List[Pálya]) =>
+      tovább(e :: többielem, (x, y + 1), háttér, pályák)
   }
 
   val ejtsd: Lépés = {
-    case (elem, (x, y)) => (elem, (x, y))
+    case (elemek: List[Pálya], honnan: (Int, Int), háttér: Pálya, pályák: List[Pálya]) =>
+
+      if (földetért(elemek, honnan, háttér)) {
+        újElemFentről(elemek, honnan, háttér, pályák)
+
+      } else {
+
+        val (x, y) = honnan
+        val _tovább = tovább(elemek, (x, y + 1), háttér, pályák)
+        ejtsd.tupled(_tovább)
+      }
   }
 
-  def ejtsdLe(elem: Pálya, többiElem: List[Pálya], honnan: (Int, Int), háttér: Pálya, pályák: List[Pálya])
-  : (List[Pálya], (Int, Int), Pálya, List[Pálya])
-  = {
-    if (földetért(elem, honnan, háttér)) {
-      következő(elem, többiElem, honnan, háttér, pályák)
+  val tovább: Lépés = {
+    case (elemek, hova, háttér: Pálya, pályák: List[Pálya]) if földetért(elemek, hova, háttér) =>
+      újElemFentről(elemek, hova, háttér, pályák)
 
-    } else {
-
-      val (x, y) = honnan
-      ejtsdLe(elem, többiElem, (x, y + 1), háttér, pályák)
-    }
+    case (e :: többiElem, (újX, újY), háttér: Pálya, pályák: List[Pálya]) =>
+      val hova = (
+        min(max(újX, 1), üresPálya.head.size - 3),
+        min(max(újY + 1, 0), üresPálya.size - 2)
+      )
+      val újPálya = rakdRá(e, hova, háttér)
+      (e :: többiElem, hova, háttér, újPálya :: pályák)
   }
 
-  def következő(e1: Pálya, többiElem: List[Pálya], hova: (Int, Int), háttér: Pálya, pályák: List[Pálya])
-  : (List[Pálya], (Int, Int), List[List[String]], List[Pálya])
-  = {
-    if (többiElem.nonEmpty) {
-      val e2 = többiElem.head
+  val újElemFentről: Lépés = {
+    case (e1 :: e2 :: többiElem, hova: (Int, Int), háttér: Pálya, pályák: List[Pálya]) =>
       val h = rakdRá(e1, hova, háttér)
       val újHáttér = if (h.dropRight(1).last.contains(" ")) h else felsőSor :: h.dropRight(2) ::: List(alsóSor)
       val újPálya = rakdRá(e2, kezdőPont, újHáttér)
       (többiElem, kezdőPont, újHáttér, újPálya :: pályák)
-    } else {
+
+    case (_, hova, háttér, pályák) =>
       (Nil, hova, háttér, pályák)
-    }
   }
 
   def rakdRá(elem: Pálya, hova: (Int, Int), pálya: Pálya): Pálya = {
