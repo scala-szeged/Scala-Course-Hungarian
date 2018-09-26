8bf5579567d924ca27fd8c569957e13785020950
jseteny <jseteny@gmail.com>
2018-09-26 12:18:51 +0200

    D3RészListaE.scala: Felesleges ág, az órán végig elemeztük. Bálint jött rá.

--- Szeptember2018/src/main/scala/d/negyedik/het/D3RészListaE.scala
+++ Szeptember2018/src/main/scala/d/negyedik/het/D3RészListaE.scala
@@ -6,23 +6,25 @@ trait ListKezelő[A] {
     def loop(aTeljes: List[A], bRész: List[A]): Boolean =
       (aTeljes, bRész) match {
 
-        case (a :: _, b :: List()) if a == b =>
-          true
+        // Felesleges ág, az órán végig elemeztük. Bálint jött rá.
+        // case (a :: _, b :: List()) if a == b =>
+        //  true
 
         case (_, List()) =>
           true
 
-        case (List(), _) =>
+        case (List(), _ :: _) =>
           false
 
         case (a :: aTovább, b :: bTovább) =>
-          if (a == b) {
+          if (a == b)
             loop(aTovább, bTovább)
-          } else if (a == részElőlről.head) {
-            loop(a :: aTovább, részElőlről)
-          } else {
+
+          else if (a == részElőlről.head)
+            loop(aTovább, részElőlről.tail)
+
+          else
             loop(aTovább, részElőlről)
-          }
       }
 
     loop(teljesElőlről, részElőlről)
@@ -58,6 +60,9 @@ object D3RészListaE {
     )
     println(
       subStringE("Valamelyik ember", "ember")
+    )
+    println(
+      subStringE("Valamelyik meber", "ember")
     )
   }
 
