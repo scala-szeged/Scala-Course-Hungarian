5d30155e0605b68bd56f48799924a5c74640cb49
jseteny <jseteny@gmail.com>
2018-11-07 09:15:17 +0100

    Első ZH feladat

---
+++ Szeptember2018/src/main/scala/h/nyolcadik/het/B1__Solution.scala
@@ -0,0 +1,17 @@
+package h.nyolcadik.het
+
+// Itt található a feladat: https://www.hackerrank.com/challenges/string-mingling/problem
+// A regisztrált felhasználók beküldhetik a megoldást, a B1__ prefix ilyenkor törlendő.
+
+object B1__Solution {
+
+  def main(args: Array[String]): Unit = {
+    val scan = scala.Console
+    val p = scan.readLine
+    val q = scan.readLine
+    println(mix(p, q))
+  }
+
+  def mix(p: String, q: String): String = {
+  }
+}
