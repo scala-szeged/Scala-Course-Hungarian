98d730c9108ab43ad3b350882d3449ad157ade6d
jseteny <jseteny@gmail.com>
2018-11-14 08:52:31 +0100

    j.tizedik.het

---
+++ Szeptember2018/src/main/scala/j/tizedik/het/J1__Solution.scala
@@ -0,0 +1,22 @@
+package j.tizedik.het
+
+// Itt található a feladat: https://www.hackerrank.com/challenges/string-mingling/problem
+// A regisztrált felhasználók beküldhetik a megoldást, a B1__ prefix ilyenkor törlendő.
+
+object J1__Solution {
+
+  def main(args: Array[String]): Unit = {
+    /*
+        val scan = scala.Console
+        val p = scan.readLine
+        val q = scan.readLine
+    */
+    val p = "abcde"
+    val q = "pqrst"
+    println(mix(p, q))
+  }
+
+  def mix(p: String, q: String): String = {
+    ""
+  }
+}
---
+++ Szeptember2018/src/main/scala/j/tizedik/het/J3__Solution.scala
@@ -0,0 +1,42 @@
+package j.tizedik.het
+
+import java.io.PrintWriter
+
+// Itt található a feladat: https://www.hackerrank.com/challenges/repeated-string/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=warmup
+// A regisztrált felhasználók beküldhetik a megoldást, a B1__ prefix ilyenkor törlendő.
+
+object J3__Solution {
+
+  def main(args: Array[String]): Unit = {
+    /*
+            val stdin = scala.Console
+            val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
+            val s = stdin.readLine
+            val n = stdin.readLine.trim.toLong
+            val result = számoldAzAkat(s, n)
+            printWriter.println(result)
+            printWriter.close()
+    */
+
+    teszt1()
+    teszt2()
+  }
+
+  def számoldAzAkat(str: String, n: Int): Int = {
+    0
+  }
+
+  def teszt1(): Unit = {
+    val str = "aba"
+    val n = 10
+    println("Elvárt eredmény: 7")
+    println(számoldAzAkat(str, n))
+  }
+
+  def teszt2(): Unit = {
+    val str = "abcac"
+    val n = 10
+    println("Elvárt eredmény: 4")
+    println(számoldAzAkat(str, n))
+  }
+}
