fb794921221757be71b5454436be330a53ae1471
jseteny <jseteny@gmail.com>
2018-12-12 15:32:02 +0100

    groups.forall

--- HackerrankFeladatokKurzusonKivul/src/main/scala/practice/algorithms/strings/SherlockAndTheValidString.scala
+++ HackerrankFeladatokKurzusonKivul/src/main/scala/practice/algorithms/strings/SherlockAndTheValidString.scala
@@ -1,44 +1,64 @@
 package practice.algorithms.strings
 
-import java.io.PrintWriter
+import java.io.{FileReader, InputStreamReader}
 
 object SherlockAndTheValidString {
   //   object Solution {
 
-  val sampleInput0 = "aabbcd" //NO
-  val sampleInput1 = "aabbccddeefghi" //NO
-  val sampleInput2 = "abcdefghhgfedecba" //YES
+  val testCase0 = "aabbcd" //NO
+  val testCase1 = "aabbccddeefghi" //NO
+  val testCase2 = "abcdefghhgfedecba" //YES
+  val testCase6 = "xxxaabbccrry" //NO
 
 
   //noinspection VariablePatternShadow
   private def isValid(str: String) = {
     val groups = str.groupBy(c => c)
-    val ((max, maxCount), (min, minCount)) = groups.foldLeft(((Int.MinValue, 0), (Int.MaxValue, 0))) {
-      case (((max, maxCount), (min, minCount)), (c, s)) =>
-        val len = s.length
-        (
-          if (len > max) (len, 1) else if (len == max) (max, maxCount + 1) else (max, maxCount),
-          if (len < min) (len, 1) else if (len == min) (min, minCount + 1) else (min, minCount)
-        )
-    }
+    val ((max, maxCount), (min, minCount)) =
+      groups.foldLeft(((Int.MinValue, 0), (Int.MaxValue, 0))) {
+        case (((max, maxCount), (min, minCount)), (c, s)) =>
+          val len = s.length
+          (
+            if (len > max) (len, 1) else if (len == max) (max, maxCount + 1) else (max, maxCount),
+            if (len < min) (len, 1) else if (len == min) (min, minCount + 1) else (min, minCount)
+          )
+      }
 
-    max == min ||
-      (max == min + 1 &&
-        (maxCount == 1 || minCount == 1))
+
+    groups.forall { case (c, s) => s.length == 1 } ||
+      (groups.count { case (c, s) => s.length == 1 } == 1 &&
+        groups.filter { case (c, s) => s.length > 1 }.forall { case (c, s) => s.length == groups.head._2.length }) ||
+      (max == min + 1 && maxCount == 1)
+  }
+
+  def eval(reader: InputStreamReader): Unit = {
+    scala.Console.withIn(reader) {
+      val s = readLine
+      println(
+        if (isValid(s)) "YES" else "NO"
+      )
+    }
   }
 
   def main(args: Array[String]) {
-    println(sampleInput0, isValid(sampleInput0))
-    println(sampleInput1, isValid(sampleInput1))
-    println(sampleInput2, isValid(sampleInput2))
 
-    /*
-        val stdin = scala.Console
-        val printWriter = new PrintWriter(sys.env("OUTPUT_PATH"))
-        val s = stdin.readLine
-        val result = isValid(s)
-        printWriter.println(result)
-        printWriter.close()
-    */
+    // eval(new InputStreamReader(System.in)) /*
+
+    println(isValid(testCase0), "NO", testCase0)
+    println(isValid(testCase1), "NO", testCase1)
+    println(isValid(testCase2), "YES", testCase2)
+    println(isValid(testCase6), "NO", testCase6)
+
+    val path = "HackerrankFeladatokKurzusonKivul/src/main/scala/practice/algorithms/strings/"
+
+    println
+    println("YES", "Test case 7.txt")
+    eval(new FileReader(path + "Test case 7.txt"))
+
+    println
+    println("YES", "Test case 13.txt")
+    eval(new FileReader(path + "Test case 13.txt"))
+
+    // */
   }
 }
